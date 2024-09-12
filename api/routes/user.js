daf998fdf7e1e22c83c04f5b66ef7ae1d244f7b5
bharat948 <bharatpatidar.002@gmail.com>
2024-09-12 11:58:58 +0530

    structured code

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require("../models/user");
-const JWT_KEY = process.env.JWT_KEY || 'your_secret_key';
 const UserController = require('../controllers/user');
 router.post("/signup", UserController.registerUser);
 
